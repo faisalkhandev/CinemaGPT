53f1dda2590f7894dfcab1cd1f2afda6dba17120
faisalKhan <faisaldev404@gmail.com>
2024-07-12 11:39:04 +0500

    add: added openai api.

--- src/Components/GptSearchBar.jsx
+++ src/Components/GptSearchBar.jsx
@@ -1,20 +1,72 @@
 import { useRef } from "react";
+import openai from "../utils/openai";
+import { options } from "../utils/constant";
 
 const GptSearchBar = () => {
     const searchText = useRef(null);
 
-    function handleGptSearchBtn() {
+    async function searchMovies(movie) {
+        const url = `https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`
+        try {
+
+            const data = await fetch(url, options)
+            const json = await data.json();
+
+            return json.results
+
+
+        } catch (error) {
+            console.log("searchAPI having issues")
+        }
+    }
+
+
+    async function handleGptSearchBtn() {
 
         console.log(searchText.current.value);
 
+        const gptQuery = "act as a movie recommendation system, suggest some movies for the query: " + searchText.current.value + ". only give me 5 names of the movies, comma separated like the example given ahead. Example Result: Hope, Goalmaal, Sholay, Don, Heera Peere"
+
+        try {
+            const gptResults = await openai.chat.completions.create({
+                messages: [{ role: 'user', content: gptQuery }],
+                model: 'gpt-3.5-turbo',
+            });
+            console.log("queryResponse::", gptResults)
+
+            console.log("gptMovies::", gptResults?.choices?.[0]?.message?.content)
+
+            const gptMovies = gptResults?.choices?.[0]?.message?.content.split(",")
+
+            //gptMovies = [Andaaz Apna, GoalMaal, Heera Peere, Jhonny]
+
+            console.log("gptMoviesSplit ,::", gptMovies)
+
+            const promiseArray = gptMovies.map((movie) => searchMovies(movie));
+
+            //promiseArr = [promise, promise, promise, promise]
+            // we will use Promise.all() to resolve all the promises 
+
+            console.log("promiseArray:::", promiseArray)
+
+            const allMoviesResult = await Promise.all(promiseArray)
+
+            console.log("promise resolved and final result", allMoviesResult)
+
+
+        } catch (error) {
+            console.log("error::", error)
+        }
 
     }
+
+
 
     return (
         <>
             <div className="flex justify-center pt-[35%] md:pt-[10%]">
                 <form
-                    className="w-full max-w-md flex"
+                    className="w-full max-w-md flex bg-slate-800 p-4 rounded"
                     onSubmit={(e) => e.preventDefault()}
                 >
                     <input
@@ -24,7 +76,7 @@ const GptSearchBar = () => {
                         ref={searchText}
                     />
                     <button
-                        className="p-4 bg-slate-700 text-white font-bold w-1/4"
+                        className="p-4 bg-black text-white font-bold w-1/4"
                         onClick={handleGptSearchBtn}
                     >
                         Search
--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,9 +1,10 @@
 import OpenAI from "openai";
 
 
-const openai = new OpenAI({
-    apiKey: import.meta.env.REACT_APP_OPENAI_KEY
-})
 
+const openai = new OpenAI({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
+});
 
 export default openai
