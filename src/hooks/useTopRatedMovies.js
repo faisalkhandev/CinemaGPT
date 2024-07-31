85eb24f01b48f76a22171ba709e7a8ad0292245e
faisalKhan <faisaldev404@gmail.com>
2024-07-31 09:12:06 +0500

    fix: fixed the .env bug.

--- src/hooks/useTopRatedMovies.js
+++ src/hooks/useTopRatedMovies.js
@@ -10,7 +10,7 @@ export const useTopRatedMovies = () => {
         method: 'GET',
         headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODUwMDFiYWQ2ODAwNjRjYTdiZTE2YzMzZjcxZjg4MiIsIm5iZiI6MTcxOTkwMDY0OC40Mzc3MjUsInN1YiI6IjY0ZWRjYTkxNTI1OGFlMDBhZGQ2Mjc2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ba-ZBzyspYXLkfeDTEmzg7il9b_-_vJSZjbVY7eeb0Q',
+            Authorization: `Bearer ${import.meta.env.VITE_MOVIES_API}`,
         },
     };
 
